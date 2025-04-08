227f87eac18e0e27705ed22732221ef8063ffb83
dudz <dzvcs@proton.me>
2025-04-08 07:53:05 -0300

    Improve layout and add responsive search bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import Layout from './components/Layout.jsx';
+import SearchBar from './components/SearchBar.jsx';
+import githubIcon from './assets/github-icon.svg';
 
 function App() {
+    function handleInputChange() {}
+
     return (
         <Layout>
-            <h1 className="text-amber-50">Hello World</h1>
-            <h1 className="text-amber-50">Hello World</h1>
+            <img src={githubIcon} alt="Github Logo" />
+            <h1 className="font-medium text-2xl text-nowrap sm:text-4xl md:text-5xl lg:text-6xl">
+                Perfil <span className="font-bold">GitHub</span>
+            </h1>
+            <SearchBar onChange={handleInputChange} />
         </Layout>
     );
 }
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,17 +1,21 @@
 const Layout = ({ children }) => {
     return (
         <div className="bg-[#1F1F1F] h-screen flex items-center justify-center">
-            <div className="bg-black w-[70%] h-[65%] flex flex-col items-center p-14 gap-10">
-                <div>
-                    {children[0]}
-                </div>
-                <div>
+            <div className="bg-black w-full md:w-[65%] h-[60%] flex flex-col items-center p-10 gap-10 text-white">
+                <div className="flex items-center gap-3 mt-6">
+                    {children[0]} {/* title and icon */}
                     {children[1]}
+                </div>
+                <div className="flex justify-center w-full ">
+                    {children[2]} {/* seachBar */}
+                </div>
+
+                <div>
+                    {children[3]} {/* result */}
                 </div>
             </div>
         </div>
-
-    )
-}
+    );
+};
 
 export default Layout;
