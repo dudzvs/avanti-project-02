b13ddea889af5f1353441e66df42df7b5b9c738e
dudz <dzvcs@proton.me>
2025-04-09 07:38:55 -0300

    add layout bg effect - add SVG detail positioned in the top-left corner

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,15 @@
+import bgDetail from '../assets/detail.svg'
+
 const Layout = ({ children, hasSearched }) => {
     return (
-        <div className="bg-[#1F1F1F] h-screen flex items-center justify-center">
-            <div className="bg-black w-full md:w-[65%] h-[100%] md:h-[737px] flex flex-col items-center p-10 gap-10 text-white">
+        <div className="bg-[#1F1F1F] h-screen flex items-center justify-center relative z-0">
+            <div className="absolute right-[-50px] top-0 w-96 h-80 bg-blue-700 opacity-70 rounded-full blur-[100px]"></div>
+            <div className="absolute left-[-50px] bottom-0 w-96 h-80 bg-blue-700 opacity-70 rounded-full blur-[100px]"></div>
+
+            <div className="bg-black w-full md:w-[65%] h-[100%] md:h-[737px] flex flex-col items-center absolute p-10 gap-10 text-white">
+                <div>
+                    <img src={bgDetail} className="absolute left-[-120px] top-[-90px] z-[-1]"/>
+                </div>
                 <div className="flex items-center gap-3 mt-6">
                     {children[0]} {/* title and icon */}
                     {children[1]}
